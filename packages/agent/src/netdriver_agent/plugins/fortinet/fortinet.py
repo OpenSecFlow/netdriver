64a24014a3935da54b0157d9b4ff320794b906d5
sam <sam@byntra.se>
2026-03-05 14:37:01 +0800

    Fortinet and Topsec plugin bug fix

--- packages/agent/src/netdriver_agent/plugins/fortinet/fortinet.py
+++ packages/agent/src/netdriver_agent/plugins/fortinet/fortinet.py
@@ -20,6 +20,7 @@ class FortinetBase(Base):
     )
 
     _CMD_CANCEL_MORE = "config system console\nset output standard\nend"
+    _CMD_END = "end"
     _SUPPORTED_MODES = [Mode.ENABLE]
 
     def get_union_pattern(self) -> re.Pattern:
@@ -48,8 +49,8 @@ class FortinetBase(Base):
         if vsys_pattern:
             vsys_match = vsys_pattern.search(prompt)
             if vsys_match and vsys_match.group(1) != self._vsys:
-                self.write_channel("end")
-                prompt = await self._get_prompt()
+                await self.write_channel(self._CMD_END)
+                prompt = await self._get_prompt(write_return=False)
         # keep decide vsys before decide mode
         self.decide_current_vsys(prompt)
         self.decide_current_mode(prompt)
