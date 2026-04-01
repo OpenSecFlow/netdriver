a3e0cbc86ff56205c30a5427b336acec7e5ae643
sam <sam@byntra.se>
2026-04-01 17:21:27 +0800

    Fortinet simunet supports VDOM

--- packages/simunet/src/netdriver_simunet/server/handlers/fortinet/fortinet_fortigate.py
+++ packages/simunet/src/netdriver_simunet/server/handlers/fortinet/fortinet_fortigate.py
@@ -32,17 +32,71 @@ class FortinetFortigateHandler(CommandHandler):
             conf_path = f"{cwd_path}/fortinet_fortigate.yml"
         self.conf_path = conf_path
         super().__init__(process)
+        self.level = []
+        self.level_type = []
+
+    @property
+    def prompt(self) -> str:
+        prompt = self.config.hostname
+        if self.level:
+            prompt = f'{prompt} ({self.level[-1]})'
+        return prompt + self.config.modes[self._mode].prompt
+
+    def switch_level(self, command: str) -> bool:
+        if command.startswith('config'):
+            commands = command.split(' ')
+            self.level_type.append(commands[0])
+            self.level.append(commands[-1])
+        elif command.startswith('edit'):
+            if self.level_type and self.level_type[-1] == 'config':
+                commands = command.split(' ')
+                self.level_type.append(commands[0])
+                self.level.append(commands[-1])
+            else:
+                return False
+        elif command == 'end':
+            if self.level_type:
+                is_edit = False
+                if self.level_type[-1] == 'edit':
+                    is_edit = True
+                self.level.pop()
+                self.level_type.pop()
+                if is_edit:
+                    self.level.pop()
+                    self.level_type.pop()
+            else:
+                return False   
+        elif command == 'next':
+            if self.level_type and self.level_type[-1] == 'edit':
+                self.level.pop()
+                self.level_type.pop()    
+            else:
+                return False
+        elif command == 'exit' and self.level:
+            return False
+        return True
+
+    def exec_cmd_in_mode(self, command: str) -> str:
+        """ Execute command in current mode """
+        self._logger.info(f"Exec [{command} in {self._mode}]")
+        if command in self.config.modes[self._mode].cmd_map:
+            if not self.switch_level(command):
+                return self.config.invalid_cmd_error
+            return self.config.modes[self._mode].cmd_map[command]
+        elif command in self.config.common_cmd_map:
+            if not self.switch_level(command):
+                return self.config.invalid_cmd_error
+            return self.config.common_cmd_map[command]
+        else:
+            return self.config.invalid_cmd_error
 
     async def switch_vsys(self, command: str) -> bool:
         return False
 
     async def switch_mode(self, command: str) -> bool:
-        if command not in self.config.modes[self._mode].switch_mode_cmds:
-            return False
-
         match self._mode:
             case Mode.ENABLE:
-                if command == "exit":
+                if command == "exit" and not self.level:
                     # logout
                     raise ClientExit
             case _:
